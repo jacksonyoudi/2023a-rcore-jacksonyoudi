bbcd87d2484e891e7496eff7dfba5e2a3de77274
jacksonyoudi <liangchangyoujackson@gmail.com>
2023-10-31 20:36:32 +0800

    fix error

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -5,6 +5,16 @@ use core::arch::asm;
 // 标准数据std 1
 const SBI_CONSOLE_PUTCHAR: usize = 1;
 
+//const SBI_SET_TIMER: usize = 0;
+// const SBI_CONSOLE_PUTCHAR: usize = 1;
+// const SBI_CONSOLE_GETCHAR: usize = 2;
+// const SBI_CLEAR_IPI: usize = 3;
+// const SBI_SEND_IPI: usize = 4;
+// const SBI_REMOTE_FENCE_I: usize = 5;
+// const SBI_REMOTE_SFENCE_VMA: usize = 6;
+// const SBI_REMOTE_SFENCE_VMA_ASID: usize = 7;
+// const SBI_SHUTDOWN: usize = 8;
+
 // sbi系统调用
 /// 在Rust中，`asm!`宏用于嵌入汇编代码。它允许你直接在Rust代码中编写汇编指令，以便与Rust代码进行混合编程。
 //
